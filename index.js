5852cb4e226a0f2ac857cccc637df798819d5b79
Luis Ariza <luismanuelariza@hotmail.com>
2022-06-02 14:47:23 -0500

    Modulos ES6

--- index.js
+++ index.js
@@ -22,12 +22,58 @@ class Student {
   }
 }
 
-class Course {
-  constructor({ name, classes = [] }) {
+function videoPlay(id) {
+  const secretUrl = "https://platziultrasecreto.com/" + id;
+  console.log("Se esta reproduciendo la url secreta " + secretUrl);
+}
+
+function videoStop(id) {
+  const secretUrl = "https://platziultrasecreto.com/" + id;
+  console.log("Pausamos la url " + secretUrl);
+}
+
+class PlatziCLass {
+  constructor({ name, videoID }) {
     this.name = name;
-    this.classes = classes;
+    this.videoID = videoID;
+  }
+  reproducir() {
+    videoPlay(this.videoID);
+  }
+  pausar() {
+    videoStop(this.videoID);
   }
 }
+
+class Course {
+  constructor({ name, classes = [] }) {
+    this._name = name;
+    this.classes = classes;
+  }
+
+  get name() {
+    return this._name;
+  }
+  set name(newName) {
+    if (newName === "Curso Malo de Programación Basica") {
+      console.error("Web... no");
+    } else {
+      this._name = newName;
+    }
+  }
+}
+
+class LearningPaths {
+  constructor({ name, courses = [] }) {
+    this.name = name;
+    this.courses = courses;
+  }
+}
+
+const class23 = new PlatziCLass({
+  name: "Javascript orientado a objetos, basado en prototipos",
+  videoID: "s4g61re6b8f8",
+});
 
 const cursoProgBasica = new Course({
   name: "Curso Gratis de Programación Básica",
@@ -40,13 +86,6 @@ const cursoCompBasica = new Course({
 const cursoIngBasico = new Course({
   name: "Inglés básico",
 });
-
-class LearningPaths {
-  constructor({ name, courses = [] }) {
-    this.name = name;
-    this.courses = courses;
-  }
-}
 
 const desarrolloWeb = new LearningPaths({
   name: "Escuela de Desarrollo Web",
