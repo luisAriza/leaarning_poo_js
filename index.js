3d88c08f37dcd82198ad6f07290680c83286f2c7
Luis Ariza <luismanuelariza@hotmail.com>
2022-06-03 15:39:43 -0500

    Uso de Object.definePropety

--- index.js
+++ index.js
@@ -13,11 +13,29 @@ const luis = {
 // console.log(Object.getOwnPropertyNames(luis));
 // console.log(Object.entries(luis));
 
-console.log(Object.getOwnPropertyDescriptors(luis));
-
-Object.defineProperty(luis, "pruebaNasa", {
-  value: "extraterrestres",
-  enumerable: true,
+Object.defineProperty(luis, "navigator", {
+  value: "Chrome",
+  enumerable: false,
   writable: true,
   configurable: true,
 });
+Object.defineProperty(luis, "editor", {
+  value: "VSCode",
+  enumerable: true,
+  writable: false,
+  configurable: true,
+});
+Object.defineProperty(luis, "terminal", {
+  value: "Bash",
+  enumerable: true,
+  writable: true,
+  configurable: false,
+});
+Object.defineProperty(luis, "pruebaNasa", {
+  value: "Extraterrestres",
+  enumerable: false,
+  writable: false,
+  configurable: false,
+});
+
+console.log(Object.getOwnPropertyDescriptors(luis));
