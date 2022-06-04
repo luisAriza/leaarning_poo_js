0c0912fd710bcfa9fa7d906741a7a5387a781429
Luis Ariza <luismanuelariza@hotmail.com>
2022-06-04 07:53:19 -0500

    Deep copy con recursividad

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
-const obj1 = {
-  a: "a",
-  b: "b",
-  c: {
-    d: "d",
-    e: "e",
-  },
-};
+function isObject(subject) {
+  return typeof subject == "object";
+}
+function isArray(subject) {
+  return Array.isArray(subject);
+}
 
-const stringify = JSON.stringify(obj1);
+function deepCopy(subject) {
+  let copySubject;
 
-const obj2 = JSON.parse(stringify);
+  const subjectIsObject = isObject(subject);
+  const subjectIsArray = isArray(subject);
+
+  if (subjectIsArray) {
+    copySubject = [];
+  } else if (subjectIsObject) {
+    copySubject = {};
+  } else {
+    return subject;
+  }
+
+  for (key in subject) {
+    const keyIsObject = isObject(subject[key]);
+    if (keyIsObject) {
+      copySubject[key] = deepCopy(subject[key]);
+    } else {
+      if (subjectIsArray) {
+        copySubject.push(subject[key]);
+      } else {
+        copySubject[key] = subject[key];
+      }
+    }
+  }
+
+  return copySubject;
+}
